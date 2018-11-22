28dd0b25832d8531115acdaf38532d3ee88ee9ca
Florian Rindlisbacher <florian.rindlisbacher@genesis.swiss>
2018-11-22 08:23:01 +0100

    refactoring

--- api/app.py
+++ api/app.py
@@ -1,6 +1,3 @@
-
-import logging
-
 from flask import Flask
 from flask_restplus import Api, Resource, fields
 from werkzeug.contrib.fixers import ProxyFix
@@ -11,9 +8,5 @@ app = Flask(__name__)
 app.wsgi_app = ProxyFix(app.wsgi_app)
 api.init_app(app)
 
-
-#logging_conf_path = os.path.normpath(os.path.join(os.path.dirname(__file__), '../logging.conf'))
-#logging.config.fileConfig(logging_conf_path)
-#log = logging.getLogger(__name__)
 
 app.run(debug=True)
