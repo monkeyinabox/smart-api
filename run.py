3c97423e58d822353208ccf97a39c92bc48c564c
Simon Wälti <simon@isorps-mbp.bfh.ch>
2018-12-21 09:39:44 +0100

    cors added

--- run.py
+++ run.py
@@ -2,9 +2,12 @@
 # 
 import logging
 from app import app
+from flask_cors import CORS
 
 logging.basicConfig(level=logging.DEBUG)
 
+cors = CORS(app, resources={r"/*": {"origins": "*"}})
 app.run(debug=True,host='0.0.0.0')
+
 
 
